fe0680d7349408b28e4c86b1f6f86199237a961d
mjbohn <mjbohn@gmail.com>
2024-07-18 14:38:18 +0200

    Add Del-Button to UserControl_Claims

--- TokenGeneratorJWT/ClaimsControl.Designer.cs
+++ TokenGeneratorJWT/ClaimsControl.Designer.cs
@@ -28,8 +28,10 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ClaimsControl));
             textBoxClaim = new TextBox();
             textBoxValue = new TextBox();
+            button1 = new Button();
             SuspendLayout();
             // 
             // textBoxClaim
@@ -48,14 +50,25 @@
             textBoxValue.Size = new Size(100, 23);
             textBoxValue.TabIndex = 3;
             // 
+            // button1
+            // 
+            button1.Image = (Image)resources.GetObject("button1.Image");
+            button1.Location = new Point(215, 2);
+            button1.Name = "button1";
+            button1.Size = new Size(24, 24);
+            button1.TabIndex = 4;
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
             // ClaimsControl
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
+            Controls.Add(button1);
             Controls.Add(textBoxValue);
             Controls.Add(textBoxClaim);
             Name = "ClaimsControl";
-            Size = new Size(212, 31);
+            Size = new Size(247, 31);
             ResumeLayout(false);
             PerformLayout();
         }
@@ -63,5 +76,6 @@
         #endregion
         public TextBox textBoxClaim;
         public TextBox textBoxValue;
+        private Button button1;
     }
 }
